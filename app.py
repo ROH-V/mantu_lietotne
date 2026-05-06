adb82aed24b75cab11ab5b3e4e1d988501a4e9a1
ROH-V <roberts.ojars.habeks@ovg.lv>
2026-05-06 16:24:40 +0300

    bazes pievienosana

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+import sqlite3
+
+def db_connection():
+    conn = sqlite3.connect("datubaze.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+def registrs():
+    db = db_connection()
+    registrs = db.execute("SELECT * FROM registrs").fetchall()
+    db.close()
+    return render_template("pieteicies.html",registrs=registrs)
