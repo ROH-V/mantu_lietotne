6017cff4d1526b890714edc9a5e14501debfa929
ROH-V <roberts.ojars.habeks@ovg.lv>
2026-05-12 16:30:46 +0300

    last update

--- app.py
+++ app.py
@@ -3,28 +3,72 @@ app = Flask(__name__)
 import sqlite3
 
 def db_connection():
-    conn = sqlite3.connect("datubaze.db")
+    conn = sqlite3.connect("mantubaze.db")
     conn.row_factory = sqlite3.Row
     return conn
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-
-def registrs():
     db = db_connection()
-    registrs = db.execute("SELECT * FROM registrs").fetchall()
+    mantubaze = db.execute("SELECT * FROM mantubaze WHERE ivn=1").fetchall()
     db.close()
-    return render_template("pieteicies.html",registrs=registrs)
+    return render_template("index.html",mantubaze=mantubaze)
 
-@app.route("/arhivs.html")
+def deregister():
+    id = request.form.get("id")
+    if id:
+        db = db_connection()
+        db.execute("DELETE FROM registrants WHERE id=?",(id,))
+        db.commit()
+        db.close()
+    #confirmed
+    db = db_connection()
+    mantubaze = db.execute("SELECT * FROM mantubaze WHERE ivn = 1").fetchall()
+    db.close()
+    return render_template("index.html",mantubaze=mantubaze)
+
+@app.route("/arhivs")
 def arhivs():
-    return render_template("arhivs.html")
+    db = db_connection()
+    mantubaze = db.execute("SELECT * FROM mantubaze WHERE ivn=0").fetchall()
+    db.close()
+    return render_template("arhivs.html",mantubaze=mantubaze)
 
 app.secret_key = "cookie_data"
 
-@app.route("/konts.html")
+
+@app.route("/konts", methods=["POST"])
+def register():
+    #validation
+    nosaukums = request.form.get("nosaukums")
+    if not nosaukums:
+        return render_template("konts.html",message=" error")
+    
+    apraksts = request.form.get("apraksts")
+    if not apraksts:
+        return render_template("konts.html",message=" error")
+    
+    kur_atrasts = request.form.get("kur_atrasts")
+    if not kur_atrasts:
+        return render_template("konts.html",message=" error")
+    
+    k_atrasts = request.form.get("k_atrasts")
+    if not k_atrasts:
+        return render_template("konts.html",message=" error")
+
+    db = db_connection()
+    db.execute("INSERT INTO mantubaze (nosaukums,apraksts, atrasanas_vieta, laiks, ivn) VALUES(?, ?, ?, ?, ?)", (nosaukums, apraksts, kur_atrasts, k_atrasts,1))
+    db.commit()
+    db.close()
+
+    #confirmed
+    db = db_connection()
+    mantubaze = db.execute("SELECT * FROM mantubaze WHERE ivn = 1").fetchall()
+    db.close()
+    return render_template("index.html",mantubaze=mantubaze)
+
+#################################
+@app.route("/konts")
 def konts():
     if not session.get("logged_in"):
         return redirect(url_for("login"))
@@ -54,3 +98,7 @@ def logout():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+#pythonanywhere
